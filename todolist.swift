b6c6b4c26e72d34ddc6365ea42bc38cfa0885013
mccartnp <nicholasmccarty252@gmail.com>
2024-08-06 22:44:40 -0400

    Added removeTask implementation

--- todolist.swift
+++ todolist.swift
@@ -17,4 +17,15 @@ class ToDoList {
     func addTask(task: MyTask) {
         tasks.append(task)
     }
+
+    func removeTask(task: MyTask) {
+        var id: Int = task.id
+
+        if let index = tasks.firstIndex(where: { task in 
+            return task.id == id 
+            }) {
+            // Step 3: Remove the task from the array
+            tasks.remove(at: index)
+}
+    }
 }
